2aa0f40942f2b1a9d8eaabce0d91428f0998cf15
arunjegathesh <121039805+arunjegathesh@users.noreply.github.com>
2023-04-13 23:28:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,28 @@ header = st.container()
 analysis = st.container()
 dashboard = st.container()
 
-
 #Caching the data for faster loading
-#@st.cache_data
+@st.cache_data
+# def get_data():
+#     df = pd.read_csv('Voodoo_Test_Business_Case.csv', sep=';')
+
+#     #fill in mode for missing values in countries 
+#     mode = df['country'].mode().to_list()[0]
+#     df['country'] = df['country'].fillna(mode)
+
+#     connection = sqlite3.connect('database.db')
+#     df.to_sql('case_table', connection, if_exists='replace')
+#     return df
+
+# df = get_data()
+
+with header: 
+    st.title('Ball Mayhem')
+    st.header('Which Ad Frequency hits better revenue? ')
+    # st.text('time 1500') #time stamp to check if streamlit web app is updated
+    st.text(' ')
+
+# with analysis:
+#     connection = sqlite3.connect('database.db') 
+#     st.subheader('Let us take a look at the data we have. ')
+#     st.write(df.head(10))

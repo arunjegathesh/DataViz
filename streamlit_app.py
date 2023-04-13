f294b8df6a37c2a9fe383d40a4be626bf9635d9c
arunjegathesh <121039805+arunjegathesh@users.noreply.github.com>
2023-04-13 23:52:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,25 +28,25 @@ df_trend = df[['tran_date', 'AOV', 'prod_cat']].copy()
 # # Convert 'tran_date' column to datetime format
 df_trend['tran_date'] = pd.to_datetime(df_trend['tran_date'], infer_datetime_format=True, errors='coerce')
 
-# # Extract month from 'tran_date' column and create a new column 'month'
-# df_trend['month'] = df_trend['tran_date'].dt.month
+# Extract month from 'tran_date' column and create a new column 'month'
+df_trend['month'] = df_trend['tran_date'].dt.month
 
-# # Group by 'prod_cat' and 'month' columns and calculate the mean of 'AOV'
-# df_trend = df_trend.groupby(['prod_cat', 'month']).mean().reset_index()
+# Group by 'prod_cat' and 'month' columns and calculate the mean of 'AOV'
+df_trend = df_trend.groupby(['prod_cat', 'month']).mean().reset_index()
 
-# # Create a FacetGrid object with 'prod_cat' column for different subplots
-# g = sns.FacetGrid(df_trend, col="prod_cat", col_wrap=3)
+# Create a FacetGrid object with 'prod_cat' column for different subplots
+g = sns.FacetGrid(df_trend, col="prod_cat", col_wrap=3)
 
-# # Map a line plot with 'tran_date' as x-axis and 'AOV' as y-axis on each subplot
-# g.map(sns.lineplot, 'tran_date', 'AOV')
+# Map a line plot with 'tran_date' as x-axis and 'AOV' as y-axis on each subplot
+g.map(sns.lineplot, 'tran_date', 'AOV')
 
-# # Set the title for each subplot
-# for ax in g.axes.flat:
-#     ax.set_title(ax.get_title().replace("prod_cat = ", "Product Category: "))
+# Set the title for each subplot
+for ax in g.axes.flat:
+    ax.set_title(ax.get_title().replace("prod_cat = ", "Product Category: "))
 
-# # Convert FacetGrid object to matplotlib figure object
-# fig = g.fig
+# Convert FacetGrid object to matplotlib figure object
+fig = g.fig
 
-# # Convert the matplotlib figure object to Streamlit pyplot object
-# st.pyplot(fig)
+# Convert the matplotlib figure object to Streamlit pyplot object
+st.pyplot(fig)
 

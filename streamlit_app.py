0c1c3010d929b9380cae5ad71e3973477c3b04b3
arunjegathesh <121039805+arunjegathesh@users.noreply.github.com>
2023-04-13 23:56:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
+import altair as alt
 
 st.set_page_config(page_title = 'Retail Sales Analysis',
                     layout='wide',
@@ -22,10 +23,13 @@ def get_data():
 
 df = get_data()
 
+# Import required libraries
+import altair as alt
+
 # Create a new dataframe with 'tran_date', 'AOV', and 'prod_cat' columns
 df_trend = df[['tran_date', 'AOV', 'prod_cat']].copy()
 
-# # Convert 'tran_date' column to datetime format
+# Convert 'tran_date' column to datetime format
 df_trend['tran_date'] = pd.to_datetime(df_trend['tran_date'], infer_datetime_format=True, errors='coerce')
 
 # Extract month from 'tran_date' column and create a new column 'month'
@@ -34,19 +38,16 @@ df_trend['month'] = df_trend['tran_date'].dt.month
 # Group by 'prod_cat' and 'month' columns and calculate the mean of 'AOV'
 df_trend = df_trend.groupby(['prod_cat', 'month']).mean().reset_index()
 
-# Create a FacetGrid object with 'prod_cat' column for different subplots
-g = sns.FacetGrid(df_trend, col="prod_cat", col_wrap=3)
+# Define a chart object with 'tran_date' as x-axis and 'AOV' as y-axis
+chart = alt.Chart(df_trend).mark_line().encode(
+    x=alt.X('tran_date', axis=alt.Axis(format='%d-%m-%Y')),
+    y=alt.Y('AOV', title='Average Order Value'),
+    color='prod_cat'
+).properties(
+    width=600,
+    height=400
+)
 
-# Map a line plot with 'tran_date' as x-axis and 'AOV' as y-axis on each subplot
-g.map(sns.lineplot, 'tran_date', 'AOV')
-
-# Set the title for each subplot
-for ax in g.axes.flat:
-    ax.set_title(ax.get_title().replace("prod_cat = ", "Product Category: "))
-
-# Convert FacetGrid object to matplotlib figure object
-fig = g.fig
-
-# Convert the matplotlib figure object to Streamlit pyplot object
-st.pyplot(fig)
+# Render the chart using Streamlit's Altair chart renderer
+st.altair_chart(chart)
 

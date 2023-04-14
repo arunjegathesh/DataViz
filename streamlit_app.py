5feca9d130f80653dea4b445801035636eae05df
arunjegathesh <121039805+arunjegathesh@users.noreply.github.com>
2023-04-14 09:24:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,17 @@ analysis = st.container()
 dashboard = st.container()
 
 @st.cache_data
+def get_data():
+    df = pd.read_csv('clean_data.csv')
+    # Convert the transaction date column to datetime format
+    df['tran_date'] = pd.to_datetime(df['tran_date'])
+
+    # Extract the year and month from the transaction date
+    df['year'] = df['tran_date'].dt.year
+    df['month'] = df['tran_date'].dt.month
+    return df
+
+df = get_data()
 
 # Define the header section
 with header:
@@ -23,84 +34,79 @@ with header:
 
 with st.sidebar:
   
-    def get_data():
-    df = pd.read_csv('clean_data.csv')  
-    return df
+#     def get_data():
+#     df = pd.read_csv('clean_data.csv')  
+#     return df
 
-    df = get_data()
+#     df = get_data()
     
     Campaign_filter = st.multiselect(label= 'Select the country',
                                 options=df['city_code'].unique(),
                                 default=df['city_code'].unique())
 
     
-with kpis:
-    st.subheader('KPIs Section Analysis')
-#    st.write('This section provides a detailed analysis of the data.')
+# with kpis:
+#     st.subheader('KPIs Section Analysis')
+# #    st.write('This section provides a detailed analysis of the data.')
   
-    def get_data():
-      df = pd.read_csv('clean_data.csv') 
-      return df
+#     def get_data():
+#       df = pd.read_csv('clean_data.csv') 
+#       return df
 
-    df = get_data()
+#     df = get_data()
 
-    df1 = df.query('campaign == @Campaign_filter')
+#     df1 = df.query('campaign == @Campaign_filter')
 
-    total_amount = float(df1['total_amt'].sum())
-    average_aov = float(df1['AOV'].mean())
-    total_qty = float(df1['Qty'].sum())
-    #total_conversions= float(df1['Total_Conversion'].sum()) 
-    #total_approved_conversions = float(df1['Approved_Conversion'].sum())
+#     total_amount = float(df1['total_amt'].sum())
+#     average_aov = float(df1['AOV'].mean())
+#     total_qty = float(df1['Qty'].sum())
+#     #total_conversions= float(df1['Total_Conversion'].sum()) 
+#     #total_approved_conversions = float(df1['Approved_Conversion'].sum())
 
-    total_amount,average_aov,total_qty = st.columns(3,gap='large')
+#     total_amount,average_aov,total_qty = st.columns(3,gap='large')
     
-# Define the analysis section
-with analysis:
-    st.subheader('Data Analysis')
-    st.write('This section provides a detailed analysis of the data.')
+# # Define the analysis section
+# with analysis:
+#     st.subheader('Data Analysis')
+#     st.write('This section provides a detailed analysis of the data.')
     
-    def get_data():
-    df = pd.read_csv('clean_data.csv')
-    connection = sqlite3.connect('database.db')
-    df.to_sql('case_table', connection, if_exists='replace')    
-    return df
+#     def get_data():
+#     df = pd.read_csv('clean_data.csv')
+#     connection = sqlite3.connect('database.db')
+#     df.to_sql('case_table', connection, if_exists='replace')    
+#     return df
 
-    df = get_data()
+#     df = get_data()
     
-    # Convert the transaction date column to datetime format
-    df['tran_date'] = pd.to_datetime(df['tran_date'])
+    
+    
+#     year_filter = st.sidebar.radio('Select year:', df['year'].unique().tolist())
+    
+#     # Filter the data based on the year filter    
+#     df = df[df['year'] == year_filter]
+    
+#     # Calculate the AOV for each month
+#     aov_monthly = df.groupby(['prod_cat', 'year', 'month']).mean().reset_index()
 
-    # Extract the year and month from the transaction date
-    df['year'] = df['tran_date'].dt.year
-    df['month'] = df['tran_date'].dt.month
-    
-    year_filter = st.sidebar.radio('Select year:', df['year'].unique().tolist())
-    
-    # Filter the data based on the year filter    
-    df = df[df['year'] == year_filter]
-    
-    # Calculate the AOV for each month
-    aov_monthly = df.groupby(['prod_cat', 'year', 'month']).mean().reset_index()
+#     # Create a selection tool for the year
+# #     year_select = alt.selection_single(
+# #         name='Year',
+# #         fields=['year'],
+# #         bind=alt.binding_select(options=aov_monthly['year'].unique().tolist())
+# #     )
 
-    # Create a selection tool for the year
-#     year_select = alt.selection_single(
-#         name='Year',
-#         fields=['year'],
-#         bind=alt.binding_select(options=aov_monthly['year'].unique().tolist())
+#     # Create an Altair chart with a dropdown menu and a tooltip
+#     aov_chart = alt.Chart(aov_monthly).mark_line().encode(
+#         x='month:N',
+#         y=alt.Y('AOV:Q', axis=alt.Axis(title='Average Order Value')),
+#         color='prod_cat:N',
+#         tooltip=['prod_cat:N', 'month:N', 'AOV:Q']
+#     ).properties(
+#         title='Seasonality of Average Order Value'
 #     )
 
-    # Create an Altair chart with a dropdown menu and a tooltip
-    aov_chart = alt.Chart(aov_monthly).mark_line().encode(
-        x='month:N',
-        y=alt.Y('AOV:Q', axis=alt.Axis(title='Average Order Value')),
-        color='prod_cat:N',
-        tooltip=['prod_cat:N', 'month:N', 'AOV:Q']
-    ).properties(
-        title='Seasonality of Average Order Value'
-    )
-
-    # Render the chart using Streamlit's Altair chart renderer
-    st.altair_chart(aov_chart)
+#     # Render the chart using Streamlit's Altair chart renderer
+#     st.altair_chart(aov_chart)
 
 # Define the dashboard section
 with dashboard:
